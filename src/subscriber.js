b0c8b5697f0c021c1fcf079f336bfd6371f7ee12
Diennd <38159404+diennguyenduy@users.noreply.github.com>
2022-06-10 09:20:38 +0200

    Subscriber save data to MongoDB

--- src/subscriber.js
+++ src/subscriber.js
@@ -1,3 +1,4 @@
+const mongodb = require('mongodb');
 const mqtt = require('mqtt')
 require('dotenv').config()
 //const client = mqtt.connect(process.env.BROKER_URL)
@@ -5,6 +6,9 @@ require('dotenv').config()
 console.log(process.env);
 const client = mqtt.connect(process.env.LOCALHOST)
 const topicName = 'aedes/test'
+
+const MongoClient = mongodb.MongoClient;
+const uri = 'mongodb://localhost/';
 
 // connect to same client and subscribe to same topic name
 client.on('connect', () => {
@@ -21,7 +25,33 @@ client.on('connect', () => {
 client.on('message', (topic, message, packet) => {
     console.log(packet, packet.payload.toString());
     if(topic === topicName) {
-     console.log(JSON.parse(message));
+        var rev_message = JSON.parse(message);
+        console.log(rev_message);
+        var temperature = rev_message.temperature;
+        var timestamp = rev_message.timestamp;
+        var sensor = rev_message.sensor;
+        async function pushInDb() {
+            const client = new MongoClient(uri, {useUnifiedTopology: true});
+            try {
+                await client.connect();
+
+                const database = client.db("TemperatureDB");
+                const temperatureColl = database.collection("temperature");
+                // create a document to be inserted
+                const doc = {
+                    value: temperature,
+                    timestamp: timestamp,
+                    sensorId: sensor,
+                    roomId: 'room1'
+                };
+
+                const result = await temperatureColl.insertOne(doc);
+                console.log(`${result.insertedCount} documents were inserted with the _id: ${result.insertedId}`,);
+            } finally {
+                await client.close();
+            }
+        }
+        pushInDb().catch(console.dir);
     }
 })
 
